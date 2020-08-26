4395d0edf200ff3769c3cc97a2b3d2e19aabda42
Bia <bpf4lc0n@gmail.com>
2020-08-26 18:17:30 +0200

    Manage service and IO operations.

---
+++ IsuCorpReservation/Res.InfraIoCLayer/DependendyContainer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Res.ApplicationLayer.Interfaces;
+using Res.ApplicationLayer.Services;
+using Res.DomainLayer.Interfaces;
+using Res.Infra.DataLayer.Context;
+using Res.Infra.DataLayer.Repositories;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Res.InfraIoCLayer
+{
+    public class DependendyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application Layer
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IRestaurantService, RestaurantService>();
+            services.AddScoped<IReserveService, ReserveService>();
+
+            // Infra Data Layer
+            services.AddScoped<ICustomerRepository, CustomerRespository>();
+            services.AddScoped<IRestaurantRepository, RestaurantRespository>();
+            services.AddScoped<IReserveRepository, ReserveRespository>();
+
+            services.AddScoped<IDatabaseInitializer, DatabaseInitializer>();
+        }
+    }
+}
