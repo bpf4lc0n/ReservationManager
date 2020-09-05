587d3935d84c45aec34a9221724cfb506bfcbdfc
Bia <bpf4lc0n@gmail.com>
2020-09-05 20:56:51 +0200

    Creating Angular components struture

---
+++ IsuCorpReservation/Res.AspAngular/ClientApp/src/app/reserves/reserve.edit/reserve.edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reserve.edit',
+  templateUrl: './reserve.edit.component.html',
+  styleUrls: ['./reserve.edit.component.css']
+})
+export class ReserveEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
